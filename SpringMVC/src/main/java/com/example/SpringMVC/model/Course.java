776cae25e24ad69d46d78ed3053a890a0da8513d
panh20020123 <tranpanh55@gmail.com>
2024-03-02 19:28:01 +0700

    lab02

---
+++ SpringMVC/src/main/java/com/example/SpringMVC/model/Course.java
@@ -0,0 +1,25 @@
+package com.example.SpringMVC.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "course_name")
+    private String courseName;
+    @Column(name = "instructor")
+    private String instructor;
+    @Column(name = "email")
+    private String email;
+}
---
+++ SpringMVC/src/main/java/com/example/SpringMVC/service/CourseService.java
@@ -0,0 +1,16 @@
+package com.example.SpringMVC.service;
+
+import com.example.SpringMVC.model.Course;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface CourseService {
+    List<Course> getAllCourses();
+    void saveCourse(Course course);
+    Course getCourseById(long id);
+    void deleteCourseById(long id);
+    Page<Course> findPaginated(int pageNum, int pageSize,
+                               String sortField,
+                               String sortDirection);
+}
